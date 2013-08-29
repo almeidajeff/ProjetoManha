868abcb12b0086c91150b7641d83cc3074e2a941
martinscosta <local@vmdes-uai.mp.rs.gov.br>
2013-08-29 12:03:10 -0300

    Acerto de bugs no models.py

--- concursos/models.py
+++ concursos/models.py
@@ -1,24 +1,25 @@
+# -*- coding: utf-8 -*-
 from django.db import models
-from django.db.models.fields import CharField, EmailField, DateTimeField, ForeignKey ,\
+from django.db.models import CharField, EmailField, DateTimeField, ForeignKey ,\
     DateField
 
 # Create your models here.
 
 class Concurso(models.Model):
     """ Dados do concurso """
-    titulo = CharField(max_lenght=200)
+    titulo = CharField(max_length=200)
     data = DateField()
 
 class Candidato(models.Model):
     """Dados do candidato que pode inscrever-se em um concurso."""
-    nome = CharField(max_lenght=100)
+    nome = CharField(max_length=100)
     email = EmailField()
 
-class Inscricao(model.Models):
+class Inscricao(models.Model):
     """Inscrições de candidatos em concursos."""
     data_hora = DateTimeField(auto_now_add=True, blank=True)
     candidato = ForeignKey(Candidato)
-    concurso = ForeignKey(Concurso)
+    concurso = models.ForeignKey(Concurso)
     class Meta:
         # O mesmo candidato não pode inscrever-se duas vezes em um mesmo concurso
         # https://docs.djangoproject.com/en/dev/ref/models/options/#unique-together
