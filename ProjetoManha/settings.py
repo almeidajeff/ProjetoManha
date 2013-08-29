ed3d3a2a1c92e1012c14a05406225c81ddc3c099
martinscosta <local@vmdes-uai.mp.rs.gov.br>
2013-08-29 11:49:42 -0300

    Ajustes iniciais do settings.py e do urls.py

--- ProjetoManha/settings.py
+++ ProjetoManha/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/homelocal/local/Documents/Aptana Studio 3 Workspace/ProjetoManha/sqlite.db',                      # Or path to database file if using sqlite3.
+        'NAME': 'concursos',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'root',
         'PASSWORD': 'root',
@@ -121,9 +121,10 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+     'django.contrib.admindocs',
+     'south',
 )
 
 # A sample logging configuration. The only tangible logging
