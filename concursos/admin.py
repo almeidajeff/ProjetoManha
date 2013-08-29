dbfbbec468bdfef32c5af3b4fc2a3eab923c44e3
carlosedb <carlosedb@hotmail.com>
2013-08-29 11:56:18 -0300

    Primeiro commit do models. Também tem o admin.py.

---
+++ concursos/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from models import *
+
+
+admin.site.register(Concurso)
+admin.site.register(Inscricao)
+admin.site.register(Candidato)
--- concursos/models.py
+++ concursos/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.db.models.fields import CharField, EmailField, DateTimeField, ForeignKey ,\
+    DateField
 
 # Create your models here.
+
+class Concurso(models.Model):
+    """ Dados do concurso """
+    titulo = CharField(max_lenght=200)
+    data = DateField()
+
+class Candidato(models.Model):
+    """Dados do candidato que pode inscrever-se em um concurso."""
+    nome = CharField(max_lenght=100)
+    email = EmailField()
+
+class Inscricao(model.Models):
+    """Inscrições de candidatos em concursos."""
+    data_hora = DateTimeField(auto_now_add=True, blank=True)
+    candidato = ForeignKey(Candidato)
+    concurso = ForeignKey(Concurso)
+    class Meta:
+        # O mesmo candidato não pode inscrever-se duas vezes em um mesmo concurso
+        # https://docs.djangoproject.com/en/dev/ref/models/options/#unique-together
+        unique_together = ('candidato', 'concurso') 
